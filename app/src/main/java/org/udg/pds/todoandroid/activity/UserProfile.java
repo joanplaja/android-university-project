a62d900213e597f48c46bfffed2faa0377d1555d
David Caceres Gonzalez <caceresdavid333@gmail.com>
2021-03-14 15:04:58 +0100

    First UI model. Cercant per internet he trobat formes de distribuir les diferents parts del perfil d'una forma estètica mes agradable i l'he començat a implementar.

---
+++ app/src/main/java/org/udg/pds/todoandroid/activity/UserProfile.java
@@ -0,0 +1,16 @@
+package org.udg.pds.todoandroid.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import org.udg.pds.todoandroid.R;
+
+public class UserProfile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+    }
+}
